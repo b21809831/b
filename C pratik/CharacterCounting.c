7e5e621d947c96059041e4e0b6af680fd1d036a8
Omer <b21809831@cs.hacettepe.edu.tr>
2019-11-03 16:23:03 +0300

    25.sayfa c

---
+++ C pratik/CharacterCounting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    long nc;
+
+    nc = 0;
+    while (getchar() != EOF)
+    {
+        ++nc;
+    printf("½ld\n" , nc);               /* code */
+    }
+    
+
+
+}
